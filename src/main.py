0085806386958c46d51fb8276ad011e646e6a863
joseluistello <jltello96@gmail.com>
2023-06-22 17:29:47 -0600

    New adds

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from schemas import Prompt
+from dotenv import load_dotenv
+import os
+import openai
 
 app = FastAPI()
 
@@ -6,3 +10,21 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+openai.api_key = os.getenv('SECRET_KEY')
+
+
+@app.post('/chat')
+def generate_response(prompt: Prompt):
+    text_length = 1000
+    gpt3_model = 'davinci'
+    response = openai.Completion.create(
+        engine=gpt3_model,
+        prompt=prompt.text,
+        max_tokens=text_length,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
+    return response['choices'][0]['text']
+                              
---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+from src.main import app
+
+client=TestClient(app)
+
+def test_index_returns_corrects():
+    response = client.get("/")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"message": "Hello World"}
+
+
+
